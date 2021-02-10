0b2c13238a7048942cf863fdb6d504fe454e1a35
hannahlmasoner <65735845+hannahlmasoner@users.noreply.github.com>
2021-02-09 20:23:39 -0600

    Add files via upload

---
+++ Experiment/Task Instructions.php
@@ -0,0 +1,25 @@
+<section class="instructions">
+    <h2 class="textcenter">Task Instructions</h2>
+    <!-- ## SET ## Change the instructions text to match your task.-->
+    <p>Welcome to the Experiment! This study will take approximately 30 minutes to complete. </p>
+
+    <p>On the next screen, you will be presented with the task instructions. Please pay attention to things we will be showing you!</p>
+
+    <p>Press ENTER when you are ready to begin.</p>
+ 
+    <div class="textcenter" id="revealRC">
+        <button class="collectorButton" type="button">Advance</button>
+    </div>
+</section>
+
+<!-- ## SET ## This ensures that participants read your instructions.
+     Participants must correctly answer something about the procedure -->
+<div class="readcheck">
+    Should you pay close attention?  (hint: Answer is in the instructions)
+    <ul>
+        <li class="MCbutton"             > I don't think so </li>
+        <li class="MCbutton"             > Nope             </li>
+        <li class="MCbutton" id="correct"> Yes              </li>
+        <li class="MCbutton"             > I can't read.    </li>
+    </ul>
+</div>
